1f24d7798b69ec92433f6f81fcda39c0733b6b96
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-04-18 17:01:59 +0500

    Team Quvonchbek

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,23 +5,41 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Navbar = () => {
     return (
         <>
-            <header>
-                <nav>
-                    <Container>
-                        <Row>
-                            <Col lg={2}>
-                                <h1 className='QPICK'>QPICK</h1>
-                            </Col>
-                            <Col lg={8}>
-                                <div className="d-flex align-items-center">
-                                    <i className="fa-solid fa-mobile-screen-button mobile-phone-icon"></i>
-                                    <p className=''>Выбрать модель телефона</p>
+            <nav>
+                <Container>
+                    <Row>
+                        <Col lg={2}>
+                            <h1 className='QPICK'>QPICK</h1>
+                        </Col>
+                        <Col lg={8}>
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-mobile-screen-button mobile-phone-icon"></i>
+                                <p className='phone-text'>Выбрать модель телефона</p>
+                            </div>
+                        </Col>
+                        <Col lg={2}>
+                            <div className="d-flex align-items-center justify-content-end">
+                                <div className="icon">
+                                    <ruby>
+                                        <rt>2</rt>
+                                        <rb>
+                                            <i class="fa-solid fa-heart icons"></i>
+                                        </rb>
+                                    </ruby>
                                 </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                </nav>
-            </header>
+                                <div className="icon">
+                                    <ruby>
+                                        <rt>1</rt>
+                                        <rb>
+                                            <i class="fa-solid fa-cart-shopping icons"></i>
+                                        </rb>
+                                    </ruby>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </nav>
         </>
     );
 }
