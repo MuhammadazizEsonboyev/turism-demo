8acffa1257f6f7fefc24457a528a0fa16bcfa286
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-04-18 15:36:20 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Cards from './components/Cards/Cards'
 
-export default function App() {
+export default function App () {
   return (
     <>
-        <Cards/>
+      <h1>Hello world</h1>
     </>
   )
 }
